31be14c73c8d7cf0504b2b08f3c316d18089b4cf
claytod5 <claytod5@protonmail.com>
2021-06-18 20:19:45 -0400

    Added binary_tree.py and binary_heap.py

---
+++ algorithms/data_structures/trees/binary_heap.py
@@ -0,0 +1,102 @@
+"""Implementation of a Binary Heap in Python."""
+
+
+class BinaryHeap:
+    def __init__(self):
+        self.heap_list = [0]  # this zero is not used; it is there for integer division
+        self.current_size = 0
+
+    def _perc_up(self, i):
+        """Percolate a node up the tree."""
+        while i // 2 > 0:
+            parent = i // 2
+
+            if self.heap_list[i] < self.heap_list[parent]:
+                # tmp = self.heap_list[parent]
+                self.heap_list[parent], self.heap_list[i] = (
+                    self.heap_list[i],
+                    self.heap_list[parent],
+                )
+            i = parent
+
+    def _perc_down(self, i):
+        while (i * 2) <= self.current_size:
+            mc = self.min_child(i)
+            if self.heap_list[i] > self.heap_list[mc]:
+                self.heap_list[i], self.heap_list[mc] = (self.heap_list[mc],  # swap
+                                                         self.heap_list[i])
+            i = mc
+
+    def _min_child(self, i):
+        left_child = (i * 2) + 1
+        if left_child > self.current_size:
+            return i * 2
+        else:
+            if self.heap_list[i * 2] < self.heap_list[left_child]:
+                return i * 2
+            else:
+                return left_child
+
+    def insert(self, k):
+        """Add a a new item to the heap."""
+        self.heap_list.append(k)
+        self.current_size = self.current_size + 1
+        self._perc_up(self.current_size)
+
+    def find_min(self):
+        """Return the item with the minimum key value, leaving item in the heap."""
+        pass
+
+    def del_min(self):
+        """Return the item with the minimum keyvalue, removing \
+        the item from the heap."""
+        pass
+
+    def isEmpty(self):
+        """Return true if the heap is empty, false otherwise."""
+        pass
+
+    def size(self):
+        """Return the number of items in the heap."""
+        pass
+
+    def build_heap(self, items):
+        """Build a new heap from a list of keys."""
+        pass
+
+    def print_tree(self):
+        """Print string prepresentation of the tree."""
+        root = self.heap_list[1]
+        heap = [str(each) for each in self.heap_list[2:]]  # get nodes as strings
+        spaces = len(heap) * 4
+        tree = str(root).center(spaces * 2) + "\n\n"  # add root to tree, centered
+        level = 1
+        while len(heap) > 2 ** level:
+            # since a complete binary heap's number of nodes per level is 2^l (where 'l'
+            # is the level), we can print each level out according to that calculation
+            r = 2 ** level
+            tmp = [each.center(spaces) for each in heap[:r]]
+            tree += "".join(tmp)
+            tree += "\n\n"
+            level += 1
+            spaces = spaces // 2
+            heap = heap[r:]  # reset heap, only including nodes beyond current level
+
+        # if 2^l was greater than the number of nodes left in the tree, concat tree and
+        # the leftover nodes
+        return tree + "".join([each.center(spaces) for each in heap])
+
+
+if __name__ == "__main__":
+    b = BinaryHeap()
+    b.insert(5)
+    b.insert(9)
+    b.insert(11)
+    b.insert(14)
+    b.insert(18)
+    b.insert(19)
+    b.insert(21)
+    b.insert(33)
+    b.insert(17)
+    b.insert(27)
+    print(b.print_tree())
