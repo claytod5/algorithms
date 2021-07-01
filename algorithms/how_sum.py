03d71be5407af8f116c41f37fd467035d9cb80c9
claytod5 <claytod5@protonmail.com>
2021-07-01 01:00:51 -0400

    add how_sum.py

---
+++ algorithms/how_sum.py
@@ -0,0 +1,19 @@
+def how_sum(n, a):
+    if n == 0:
+        return []
+    elif n < 0:
+        return None
+
+    for each in a:
+        recur = how_sum(n - each, a)
+        if recur is not None:
+            return [each] + recur
+
+    return None
+
+
+if __name__ == "__main__":
+    print(how_sum(7, [7, 5, 2, 8, 4]))
+    print(how_sum(8, [2, 3, 5]))
+    print(how_sum(7, [2, 4]))
+    print(how_sum(0, [1, 2, 3]))
