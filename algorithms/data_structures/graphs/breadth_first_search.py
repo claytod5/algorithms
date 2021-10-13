0380aec36d9ca472dc3bca7f29232e1e0764b4d2
claytod5 <claytod5@protonmail.com>
2021-10-13 10:05:01 -0400

    Refactor bfs to not include backtracing

--- algorithms/data_structures/graphs/breadth_first_search.py
+++ algorithms/data_structures/graphs/breadth_first_search.py
@@ -2,35 +2,18 @@
 
 
 from collections import deque
-from backtrace import backtrace
 
 
-def breadth_first_search(graph, start, end):
-    parent = dict()
+def breadth_first_search(graph, start):
     search_queue = deque()
     search_queue += start
     visited = set()
 
     while search_queue:
         node = search_queue.popleft()
-        if node == end:
-            return backtrace(parent, start, end)
         for each in graph[node]:
             if each not in visited:
-                search_queue += graph[node]
-                parent[each] = node
-                visited.add(node)
+                search_queue += graph[each]
+        visited.add(node)
 
-    return False
-
-
-if __name__ == "__main__":
-    graph = {
-        "u": ["v", "x", "w"],
-        "x": ["u", "v", "w", "y"],
-        "v": ["u", "x", "w"],
-        "w": ["v", "y", "z", "x", "u"],
-        "z": ["w", "y"],
-        "y": ["z", "w", "x"],
-    }
-    print(breadth_first_search(graph, "z", "v"))
+    return visited
